e49aed8ec63297f80e01f9fd26d83173b6a55e2f
Pratheep Wongho <pratheepw@hotmail.com>
2020-11-24 09:58:43 +0700

    เปลี่ยนชื่อฟอร์มเป็น M08

--- M08/Program.cs
+++ M08/Program.cs
@@ -16,7 +16,7 @@ namespace M08
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new XtraForm1());
+            Application.Run(new M08());
         }
     }
 }
